4e84248329021e44633c8e5863e70c9030199285
HenrySchiffUP <schiff26@up.edu>
2024-11-01 19:16:57 -0700

    added camera controls (arrow keys and +/-)

--- gamejam2024/camera_2d.gd
+++ gamejam2024/camera_2d.gd
@@ -9,3 +9,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down") * 10
+	
+	if Input.is_action_just_pressed("camera_zoom_in"):
+		zoom *= 1.2
+		
+	if Input.is_action_just_pressed("camera_zoom_out"):
+		zoom *= 1 / 1.2
