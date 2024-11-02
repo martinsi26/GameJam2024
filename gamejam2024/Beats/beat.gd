674e4c48d279776344ce179044e6775014396359
Chris Yee <candjvgames@gmail.com>
2024-11-02 14:11:49 -0700

    initial thoughts

--- gamejam2024/Beats/beat.gd
+++ gamejam2024/Beats/beat.gd
@@ -1,12 +1,27 @@
 extends Node
 
 @onready var audio : AudioStreamPlayer = $BeatsPerMinute
-@export var level = 0
+@export var level = 1 # change to per level/map
+@export var active = true
+@export var control: Vector2
 
 #beats.level = 1
 #beats._ready()
 
 func _ready() -> void:
-	while(1):
+	while(active):
+		#control = true
 		audio.play()
 		await get_tree().create_timer(2.0 / (level * 2)).timeout
+		
+
+func _process(delta: float) -> void:
+	var time = audio.get_playback_position() + AudioServer.get_time_since_last_mix()
+	# Compensate for output latency.
+	time -= AudioServer.get_output_latency()
+	print("Time is: ", time)
+	print("Control is: ", control)
+	
+	
+	if time >= (2.0 / (level * 2)) && !control:
+		pass
