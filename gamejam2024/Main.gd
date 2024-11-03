97f1faba532af9d0732bd6b8a215c707ac550fba
martinsi26 <martinsi26@up.edu>
2024-11-03 15:21:41 -0800

    Updates

--- gamejam2024/Main.gd
+++ gamejam2024/Main.gd
@@ -3,6 +3,13 @@ extends Node2D
 @onready var screen_wipe = $CanvasLayer/Sprite2D
 var screen_wipe_playing = false
 
+var water_full = preload()
+var water_half = preload()
+var water_empty = preload()
+
+@onready var water_bar = [$CanvasLayer/Control/Water_1, $CanvasLayer/Control/Water_2, $CanvasLayer/Control/Water_3, $CanvasLayer/Control/Water_4, $CanvasLayer/Control/Water_5]
+var latest_water = 5
+
 var current_map = 0
 
 var total_coins = 0
@@ -64,6 +71,9 @@ func enter_map0():
 	current_map_instance = instance0
 	add_child(instance0)
 	
+	instance0.get_node("Fish").water_changed.connect(update_water_display)
+	instance0.get_node("Fish").water_reset.connect(reset_water_bar)
+	
 	var coin = coin_scene.instantiate()
 	instance0.get_node("Coins").add_child(coin)
 	coin.set_starting_tile(Vector3(-6, -1, 1))
@@ -77,6 +87,9 @@ func enter_map1():
 	var instance1 = map1.instantiate()
 	current_map_instance = instance1
 	add_child(instance1)
+	
+	instance1.get_node("Fish").water_changed.connect(update_water_display)
+	instance1.get_node("Fish").water_reset.connect(reset_water_bar)
 	
 	var fox = fox_scene.instantiate()
 	instance1.get_node("Foxes").add_child(fox)
@@ -103,10 +116,13 @@ func enter_map1():
 	instance1.get_node("Fish").finished_map.connect(finished)
 	emit_signal("set_starting_values", Vector2i(-1, 0), 0)
 	
-func enter_map2():
+func enter_map4():
 	var instance2 = map2.instantiate()
 	current_map_instance = instance2
 	add_child(instance2)
+	
+	instance2.get_node("Fish").water_changed.connect(update_water_display)
+	instance2.get_node("Fish").water_reset.connect(reset_water_bar)
 	
 	var shark = shark_scene.instantiate()
 	instance2.get_node("Sharks").add_child(shark)
@@ -138,10 +154,13 @@ func enter_map2():
 	coin3.call_pickup.connect(instance2.get_node("Fish").pickup)
 	coin4.call_pickup.connect(instance2.get_node("Fish").pickup)
 	
-func enter_map3():
+func enter_map5():
 	var instance3 = map3.instantiate()
 	current_map_instance = instance3
 	add_child(instance3)
+	
+	instance3.get_node("Fish").water_changed.connect(update_water_display)
+	instance3.get_node("Fish").water_reset.connect(reset_water_bar)
 	
 	var shark = shark_scene.instantiate()
 	instance3.get_node("Sharks").add_child(shark)
@@ -162,10 +181,13 @@ func enter_map3():
 	instance3.get_node("Fish").finished_map.connect(finished)
 	emit_signal("set_starting_values", Vector2i(-1, 0), 0)
 	
-func enter_map4():
+func enter_map3():
 	var instance4 = map4.instantiate()
 	current_map_instance = instance4
 	add_child(instance4)
+	
+	instance4.get_node("Fish").water_changed.connect(update_water_display)
+	instance4.get_node("Fish").water_reset.connect(reset_water_bar)
 	
 	var bear = bear_scene.instantiate()
 	instance4.get_node("Bears").add_child(bear)
@@ -206,10 +228,13 @@ func enter_map4():
 	instance4.get_node("Fish").finished_map.connect(finished)
 	emit_signal("set_starting_values", Vector2i(-1, 0), 0)
 	
-func enter_map5():
+func enter_map2():
 	var instance5 = map5.instantiate()
 	current_map_instance = instance5
 	add_child(instance5)
+	
+	instance5.get_node("Fish").water_changed.connect(update_water_display)
+	instance5.get_node("Fish").water_reset.connect(reset_water_bar)
 
 	var bear = bear_scene.instantiate()
 	instance5.get_node("Bears").add_child(bear)
@@ -241,6 +266,9 @@ func enter_map6():
 	var instance6 = map6.instantiate()
 	current_map_instance = instance6
 	add_child(instance6)
+	
+	instance6.get_node("Fish").water_changed.connect(update_water_display)
+	instance6.get_node("Fish").water_reset.connect(reset_water_bar)
 	
 	var fox = fox_scene.instantiate()
 	instance6.get_node("Foxes").add_child(fox)
@@ -310,6 +338,9 @@ func enter_map7():
 	var instance7 = map7.instantiate()
 	current_map_instance = instance7
 	add_child(instance7)
+	
+	instance7.get_node("Fish").water_changed.connect(update_water_display)
+	instance7.get_node("Fish").water_reset.connect(reset_water_bar)
 
 	var shark1 = shark_scene.instantiate()
 	instance7.get_node("Shark1").add_child(shark1)
@@ -371,6 +402,9 @@ func enter_map8():
 	var instance8 = map8.instantiate()
 	current_map_instance = instance8
 	add_child(instance8)
+	
+	instance8.get_node("Fish").water_changed.connect(update_water_display)
+	instance8.get_node("Fish").water_reset.connect(reset_water_bar)
 	
 	var bear1 = bear_scene.instantiate()
 	instance8.get_node("Bear1").add_child(bear1)
@@ -456,7 +490,6 @@ func enter_map8():
 	instance8.get_node("Fish").finished_map.connect(finished)
 	emit_signal("set_starting_values", Vector2i(-1, 0), 0)
 
-	
 	var coin1 = coin_scene.instantiate()
 	instance8.get_node("Coin1").add_child(coin1)
 	coin1.set_starting_tile(Vector3(-7, -17, 2))
@@ -480,3 +513,16 @@ func finished():
 	print(current_map)
 	print(enter_map_functions[current_map])
 	enter_map_functions[current_map].call()
+	
+func update_water_display(current_water):
+	if latest_water >= 0:
+		if water_bar[latest_water-1].texture == water_full:
+			water_bar[latest_water-1].texture = water_half
+		else:
+			water_bar[latest_water-1].texture = water_empty
+			latest_water -= 1
+			
+func reset_water_bar():
+	for i in water_bar:
+		i.texture = water_full
+	latest_water = 5
