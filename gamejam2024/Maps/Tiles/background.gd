61ef68a76d8e4cb76bb28f88af22aae855377729
Chris Yee <candjvgames@gmail.com>
2024-11-03 13:07:21 -0800

    oop

--- gamejam2024/Maps/Tiles/background.gd
+++ gamejam2024/Maps/Tiles/background.gd
@@ -10,12 +10,10 @@ func _ready() -> void:
 	start_y = bg.position.y
 	
 func move_tiles():
-	bg.position.x -= 0.5
-	bg.position.y -= 0.5
+	bg.position.x -= 0.4
 	
-	if bg.position.x == start_x - 32:
+	if bg.position.x == start_x - 64:
 		bg.position.x = start_x
-		bg.position.y = start_y
 	
 func _process(delta: float) -> void:
 	move_tiles()
